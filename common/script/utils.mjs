066ed12093214e15e9840b9bc9fc6e7d55fc82db
iselinsj <iselinsimone@icloud.com>
2024-09-04 15:03:14 +0200

    task2.1 assign

--- common/script/utils.mjs
+++ common/script/utils.mjs
@@ -1,12 +1,9 @@
-"use strict";
 
 let DOMTextOut = null;
 export const newLine = "<br/>";
-
 export function initPrintOut(aDomElement) {
   DOMTextOut = aDomElement;
 }
-
 export function printOut(aText) {
   aText = aText.replace(newLine, "");
   if (DOMTextOut.innerHTML.length !== 0) {
---
+++ module_2/task_2-1/Task2-1.js
@@ -0,0 +1,11 @@
+ import  {initPrintOut, printOut, newLine } from "/DAT101/common/script/utils.mjs";
+    initPrintOut(document.getElementById("txtOut"));
+    
+    let x = 9;
+    let y = 7;
+    let z = x + y;
+    let valuePI = 3.14;
+    printOut(y, z, valuePI);
+    printOut ("Hei");
+    
+    
