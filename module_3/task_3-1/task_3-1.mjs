63cb883fbf1bd48668a7ebb5f554db80d6ded50d
iselinsj <iselinsimone@icloud.com>
2024-10-30 14:21:32 +0100

    Commited task 3.2

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -5,39 +5,127 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+
+let wakeUpTime = 8;
+
+if (wakeUpTime == 7) {
   printOut("I can take the bus to school.");
+
+}else if(wakeUpTime == 8){
+  printOut("i can take the train");
+
+  }else{
+  printOut("i take the car to school");
 }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let integer = 3;
+printOut("Value = " + integer.toString());
+if(integer<0){
+  printOut("Value is Negativ");
+} else if(integer==0){
+  printOut("Value is Zero");
+} else {
+  printOut("Value is Positive ");
+}
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const imageMinSize = 4;
+const imageSize = Math.ceil(Math.random() * 8);
+printOut("The image size is: " + imageSize + "MP.");
+if(imageSize >= imageMinSize) {
+  printOut("Thank you");
+}else {
+  printOut("The image is too small");
+}
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+printOut("Submitted File Size: " + imageSize.toString());
+if (imageSize >=6) {
+  printOut("Image is too large");
+} else if (imageSize >=4) {
+printOut("Thank you")
+} else {
+  printOut("The image is too small")
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList = ["January", "February", "Mars", "April", "Mai",
+  "Jun", "Juli", "August", "September", "October", "November", "December"];
+
+  const noOfMonth = monthList.length;
+  const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+  printOut("Month Is = " + monthName);
+
+  if (monthName.includes("r")) {
+printOut("You must take vitamin D");
+  } else {
+    printOut("You do not need to take vitamin D");
+  }
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let daysOfMonth = null;
+switch (monthName) {
+  case "January":
+  case "Mai":
+  case "Mars":
+  case "Juli":
+  case "August":
+  case "October":
+  case "December":
+    daysOfMonth = 31;
+break;
+  case "April":
+  case "Jun":
+  case "September":
+  case "November":
+   daysOfMonth = 30;
+  break;
+  default:
+    "February"
+
+    daysOfMonth = 28;
+}
+printOut("There is " + daysOfMonth  + " Days In  " + monthName);
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthlist = ["January", "February", "Mars", "April", "Mai",
+  "Jun", "Juli", "August", "September", "October", "November", "December"];
+
+  const noofMonth = monthlist.length;
+  const monthname = monthlist[Math.floor(Math.random() * noofMonth)];
+
+  if (monthName.includes("r")) {
+printOut("Gallery is not open in " + monthname);
+  } else {
+    printOut("Gallery is open in " + monthname + " , welcome!");
+  }
+  
+
 printOut(newLine);
--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -9,12 +9,30 @@ printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const answerNumber = 45;
+let guessNumber = 0;
+while(answerNumber !== guessNumber){
+    guessNumber = Math.ceil(Math.random() * 60);
+
+}
+printOut("Guess number = " + guessNumber.toString());
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+guessNumber = 0;
+let guessCount = 0;
+const startTime = Date.now();
+while(answerNumber !== guessNumber){
+    guessCount++;
+    guessNumber = Math.ceil(Math.random() * 1000000);
+
+}
+const endTime = Date.now();
+const timeUsed = endTime - startTime;
+printOut("Antall gjetninger " + guessCount.toString() + " tok " + timeUsed.toString() + " millisekunder ");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
@@ -34,8 +52,48 @@ printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("Dice rolling: ");
+
+const d1 = Math.ceil(Math.random() * 6);
+const d2 = Math.ceil(Math.random() * 6);
+const d3 = Math.ceil(Math.random() * 6);
+const d4 = Math.ceil(Math.random() * 6);
+const d5 = Math.ceil(Math.random() * 6);
+const d6 = Math.ceil(Math.random() * 6);
+
+let diceThrow = "";
+diceThrow += d1.toString() + ",";
+diceThrow += d2.toString() + ",";
+diceThrow += d3.toString() + ",";
+diceThrow += d4.toString() + ",";
+diceThrow += d5.toString() + ",";
+diceThrow += d6.toString();
+
+printOut("diceThrow: " + diceThrow);
+
+const count1 = (diceThrow.match(/1/g) || "").length;
+const count2 = (diceThrow.match(/2/g) || "").length;
+const count3 = (diceThrow.match(/3/g) || "").length;
+const count4 = (diceThrow.match(/4/g) || "").length;
+const count5 = (diceThrow.match(/5/g) || "").length;
+const count6 = (diceThrow.match(/6/g) || "").length;
+
+
+let diceCount = "";
+diceCount += count1.toString() + ",";
+diceCount += count2.toString() + ",";
+diceCount += count3.toString() + ",";
+diceCount += count4.toString() + ",";
+diceCount += count5.toString() + ",";
+diceCount += count6.toString() + ",";
+printOut("diceCount: " + diceCount);
+
+const equals1 = (diceCount.match(/1/g) || "").length;
+const equals6 = (diceCount.match(/6/g) || "").length;
+printOut("equals1: " + equals1.toString());
+printOut("equals6: " + equals6.toString());
 printOut(newLine);
+
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
