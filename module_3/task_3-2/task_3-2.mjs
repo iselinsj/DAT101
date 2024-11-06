9605461a948e0b4658497338387060d48ea7442a
iselinsj <iselinsimone@icloud.com>
2024-11-06 16:07:53 +0100

    Oppgave 3.2 1

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -5,7 +5,16 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
-printOut(newLine);
+
+let txtLine1 = "";
+let txtLine2 = "";
+
+for(let i = 1, j = 10; i<= 10; i++, j--) {
+    txtLine1 += " " + i.toString();
+    txtLine2 += " " + j.toString();
+}
+printOut(newLine + txtLine1 + newLine);
+printOut(txtLine2 + newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
@@ -37,23 +46,65 @@ printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let txtPrintValue = "";
+
+for(let testNumber = 2; testNumber < 200; testNumber++){
+    let isNotPrime = false;
+    let divide = 2;
+    while((isNotPrime === false) && (divide <testNumber)){
+        isNotPrime = (testNumber % divide) === 0;
+        divide++;
+    }
+    if(isNotPrime === false){
+        txtPrintValue += testNumber.toString() + " ";
+    }
+}
+printOut(newLine + txtPrintValue + newLine);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let PrintValue = "";
+for(let row = 1; row <= 9; row++){
+    for(let col = 1; col <= 7; col++){
+        PrintValue += "K" + col.toString() + "R" + row.toString() + "";
+    } PrintValue += newLine;
+}
+
+printOut(PrintValue);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let txtValue = "";
+const gradePoints = Math.floor(Math.random() * 237);
+const gradePercent = gradePoints / 236 * 100;
+let grade;
+if(gradePercent <= 40){
+    grade = "F";
+} else if (gradePercent <= 52){
+    grade = "E";
+} else if(gradePercent <= 64){
+    grade = "D";
+} else if(gradePercent <= 76){
+    grade = "C";
+} else if(gradePercent <= 88){
+    grade = "B";
+} else if(gradePercent <= 100){
+    grade = "A";
+}
+
+txtValue += newLine + "Eleven fikk totalt " + gradePoints.toString() + " poeng. ";
+txtValue += newLine + "Dette tilsvarer " + gradePercent.toFixed(0) + " % av antall mulige poeng, og gir derfor eleven karakteren " + grade + ".";
+printOut(txtValue);
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Dice rolling: ");
-
+txtPrintValue = "";
 const d1 = Math.ceil(Math.random() * 6);
 const d2 = Math.ceil(Math.random() * 6);
 const d3 = Math.ceil(Math.random() * 6);
@@ -88,12 +139,80 @@ diceCount += count5.toString() + ",";
 diceCount += count6.toString() + ",";
 printOut("diceCount: " + diceCount);
 
-const equals1 = (diceCount.match(/1/g) || "").length;
-const equals6 = (diceCount.match(/6/g) || "").length;
-printOut("equals1: " + equals1.toString());
-printOut("equals6: " + equals6.toString());
-printOut(newLine);
+	 String.prototype.numberOfOccurence = function (aSubstring) {
+		 const listStrings = this.split(aSubstring);
+		 return listStrings.length - 1;
+	 };
 
+	 let fullStraight = false;
+	 let threePairs = false;
+	 let tower = false;
+	 let yatzy = false;
+	 let counter = 0;
+
+	 do {
+		const d1 = Math.ceil(Math.random() * 6 );
+		const d2 = Math.ceil(Math.random() * 6 );
+		const d3 = Math.ceil(Math.random() * 6 );
+		const d4 = Math.ceil(Math.random() * 6 );
+		const d5 = Math.ceil(Math.random() * 6 );
+		const d6 = Math.ceil(Math.random() * 6 );
+		const diceRoll = d1 + ", " + d2 + ", " + d3 + ", " + d4 + ", " + d5 + ", " + d6; 
+		
+		const c1 = diceRoll.numberOfOccurence("1");
+		const c2 = diceRoll.numberOfOccurence("2");
+		const c3 = diceRoll.numberOfOccurence("3");
+		const c4 = diceRoll.numberOfOccurence("4");
+		const c5 = diceRoll.numberOfOccurence("5");
+		const c6 = diceRoll.numberOfOccurence("6");
+		const countDice = c1 + ", " + c2 + ", " + c3 + ", " + c4 + ", " + c5 + ", " + c6; 
+
+		const o1 = countDice.numberOfOccurence("1");
+		const o2 = countDice.numberOfOccurence("2");
+		const o3 = countDice.numberOfOccurence("3");
+		const o4 = countDice.numberOfOccurence("4");
+		const o5 = countDice.numberOfOccurence("5");
+		const o6 = countDice.numberOfOccurence("6");
+		const occurenceDice = o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5 + ", " + o6; 
+
+	
+	if (!yatzy){
+		yatzy = (o6 === 1)
+		if (yatzy) {
+		txtPrintValue += diceRoll + newLine;
+		txtPrintValue += "Yatzy!" + newLine;
+		txtPrintValue += " Det tok " + counter + " forsøk.";
+		}
+
+	}
+	 if(!fullStraight) {
+		fullStraight = (o1 === 6);
+		if (fullStraight) {
+			txtPrintValue += diceRoll + newLine;
+			txtPrintValue += "Full straight!" + newLine;
+			txtPrintValue += "Det tok " + counter + " forsøk." + newLine + newLine;
+		}
+	} if(!threePairs) {
+		threePairs = (o2 === 3);
+		if (threePairs) {
+			txtPrintValue += diceRoll + newLine;
+			txtPrintValue += "Tre par!" + newLine;
+			txtPrintValue += "Det tok " + counter + " forsøk." + newLine + newLine;
+		}
+	} if(!tower) {
+		tower = (o2 === 1 && o4 === 1);
+		if (tower) {
+			txtPrintValue += diceRoll + newLine;
+			txtPrintValue += "Tårn!" + newLine;
+			txtPrintValue += "Det tok " + counter + " forsøk." + newLine + newLine;
+		}
+	}
+	
+	counter++;
+	}
+
+	while (yatzy === false) {
+		printOut(newLine + txtPrintValue + newLine)}
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
--- module_3/task_3-3/task_3-3.mjs
+++ module_3/task_3-3/task_3-3.mjs
@@ -24,12 +24,75 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const ETempratureType = {Celsius: 1, Fahrenheit: 2, Kelvin: 3}
+function convertTemprature(aTemprature, aType) {
+    let Fahrenheit = 0;
+    let Celsius = 0;
+    let Kelvin = 0;
+    switch(aType){
+        case ETempratureType.Celsius:
+        printOut("Convert from Celsius");
+        //convert to fahrenheit
+        //fahrenheit = (Kelvin - 273.15)*9/5 + 32;
+            Celsius = aTemprature;
+            Fahrenheit = (Celsius * 9/5) + 32;
+            Kelvin = Celsius + 237.15;
+            break; 
+        case ETempratureType.Fahrenheit:
+            printOut("Convert from Fahrenheit");
+            break;
+        case ETempratureType.Kelvin:
+            printOut("Convert from Kelvin");
+            break;
+
+    } //end switch
+
+    printOut("Celsius = " + Celsius.toFixed());
+    printOut("Fahrenheit = " + Fahrenheit.toFixed());
+    printOut("Kelvin = " + Kelvin.toFixed());
+} //end function
+
+convertTemprature(37, ETempratureType.Celsius);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function calculateNetPrice(aPrice, aTaxGroup){
+    let net = NaN;
+    let TaxGroup = aTaxGroup.toUpperCase();
+    let vat = NaN;
+
+    printOut("taxGroup " + TaxGroup);
+
+    switch(TaxGroup){
+        case "NORMAL": 
+        vat = 25;
+    }
+    if(!Number.isNaN(vat)){
+    net = (100 * aPrice) / (vat + 100);
+    }else{
+        net = NaN
+    }
+
+    return net; 
+}
+const netPrice1 = calculateNetPrice(100, "normal");
+if(Number.isNaN(netPrice1)){
+    printOut("Uknown VAT group");
+}else{
+    printOut("netPrice1 = " + netPrice1.toFixed(2));
+}
+
+const netPrice2 = calculateNetPrice(100, "goblins");
+if(Number.isNaN(netPrice2)){
+    printOut("Uknown VAT group");
+}else{
+    printOut("netPrice2 = " + netPrice2.toFixed(2));
+}
+
+printOut("netPrice1 = " + netPrice1.toFixed());
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
@@ -44,6 +107,35 @@ printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+function testIfMathIsFun(){
+    let op = 1;
+    let line = 2;
+    //left side
+    let ok = false;
+    do{
+    let sumLeft = 0;
+    for(let left = 0; left < line + 1; left++){
+        sumLeft += op;
+        op++;
+    }
+
+    let sumRight = 0;
+    for(let right = 0; right < line; right++){
+        sumRight += op;
+        op++;
+    }
+
+    if(sumLeft !== sumRight){
+        ok = false;
+    }else{
+        ok = true;
+        //på en eller annen måte må vi stoppe functions
+    } 
+    line++;
+}while (!ok);
+
+}
 printOut("Replace this with you answer!");
 printOut(newLine);
 
